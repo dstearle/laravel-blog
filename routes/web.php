2e5c007ded73225c155d4e4d2d0b1f10ea1b907d
dstearle <dallas.st.earle@gmail.com>
2019-11-06 12:24:44 -0800

    Made basic examples for making routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+## Basic Examples ##
+
+    // For: http://laravel-blog.me/hello
+    // Route::get('/hello', function () {
+    //     return '<h1>Hello World!</h1>';
+    // });
+
+    // For: http://laravel-blog.me/users/dallas
+    // Route::get('/users/{id}', function ($id) {
+    //     return 'This is user ' . $id;
+    // });
+
+    // For: http://laravel-blog.me/users/2/dallas
+    // Route::get('/users/{id}/{name}', function ($id, $name) {
+    //     return 'This is user ' . $name . ' with an ID of ' . $id;
+    // });
+
+# End Of Basic Examples
+
+// For: http://laravel-blog.me/about
+Route::get('/about', function () {
+    return view('pages.about');
+});
